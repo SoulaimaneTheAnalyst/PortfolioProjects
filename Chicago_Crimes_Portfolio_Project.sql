611e5a41213adabbe62dd299439a3c0f77dcdc8a
SoulaimaneTheAnalyst <139348614+SoulaimaneTheAnalyst@users.noreply.github.com>
2023-07-12 12:47:42 +0100

    Update Chicago_Crimes_Portfolio_Project.sql

--- Chicago_Crimes_Portfolio_Project.sql
+++ Chicago_Crimes_Portfolio_Project.sql
@@ -1,8 +1,27 @@
--- SELECT ID, CASE_NUMBER, DATE, PRIMARY_TYPE, DESCRIPTION, LOCATION_DESCRIPTION, ARREST, DOMESTIC, YEAR FROM ChicagoCrimeData
--- SELECT DISTINCT(COUNT(CASE_NUMBER)) FROM ChicagoCrimeData (533 cases) -- Problem 1: Find the total number of crimes recorded in the CRIME table.
--- SELECT COMMUNITY_AREA_NAME, PER_CAPITA_INCOME FROM ChicagoCensusData WHERE PER_CAPITA_INCOME<11000 -- Problem 2: List community areas with per capita income less than 11000.
--- SELECT CASE_NUMBER FROM ChicagoCrimeData WHERE DESCRIPTION LIKE '%MINOR%' --Problem 3: List all case numbers for crimes involving minors?(children are not considered minors for the purposes of crime analysis)
--- SELECT CASE_NUMBER FROM ChicagoCrimeData WHERE DESCRIPTION LIKE '%CHILD%' AND PRIMARY_TYPE='KIDNAPPING' -- Problem 4: List all kidnapping crimes involving a child?
+-- Problem 1: Find the total number of crimes recorded in the CRIME table.
+
+SELECT DISTINCT(COUNT(CASE_NUMBER)) 
+FROM ChicagoCrimeData (533 cases) 
+
+-- Problem 2: List community areas with per capita income less than 11000.
+
+SELECT COMMUNITY_AREA_NAME, PER_CAPITA_INCOME 
+FROM ChicagoCensusData
+WHERE PER_CAPITA_INCOME<11000 
+
+-- Problem 3: List all case numbers for crimes involving minors?(children are not considered minors for the purposes of crime analysis)
+
+SELECT CASE_NUMBER 
+FROM ChicagoCrimeData 
+WHERE DESCRIPTION 
+LIKE '%MINOR%' 
+
+-- Problem 4: List all kidnapping crimes involving a child?
+
+SELECT CASE_NUMBER 
+FROM ChicagoCrimeData 
+WHERE DESCRIPTION LIKE '%CHILD%' AND PRIMARY_TYPE='KIDNAPPING' 
+
 -- SELECT DISTINCT(PRIMARY_TYPE), LOCATION_DESCRIPTION FROM ChicagoCrimeData WHERE LOCATION_DESCRIPTION LIKE '%SCHOOL%' -- Problem 5: What kinds of crimes were recorded at schools?
 -- SELECT AVG(SAFETY_SCORE) AS AVERAGE_SAFETY_SCORE FROM ChicagoPublicSchools -- Problem 6: what is the average safety score in chicago public schools
 -- select TOP(5) COMMUNITY_AREA_NAME, PERCENT_HOUSEHOLDS_BELOW_POVERTY from ChicagoCensusData ORDER BY PERCENT_HOUSEHOLDS_BELOW_POVERTY DESC -- Problem 7: List 5 community areas with highest % of households below poverty line
