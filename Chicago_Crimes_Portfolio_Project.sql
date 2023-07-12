b232327b0c44542d811e521ee5eb754e8806f356
SoulaimaneTheAnalyst <139348614+SoulaimaneTheAnalyst@users.noreply.github.com>
2023-07-12 12:43:41 +0100

    Add files via upload

---
+++ Chicago_Crimes_Portfolio_Project.sql
@@ -0,0 +1,11 @@
+-- SELECT ID, CASE_NUMBER, DATE, PRIMARY_TYPE, DESCRIPTION, LOCATION_DESCRIPTION, ARREST, DOMESTIC, YEAR FROM ChicagoCrimeData
+-- SELECT DISTINCT(COUNT(CASE_NUMBER)) FROM ChicagoCrimeData (533 cases) -- Problem 1: Find the total number of crimes recorded in the CRIME table.
+-- SELECT COMMUNITY_AREA_NAME, PER_CAPITA_INCOME FROM ChicagoCensusData WHERE PER_CAPITA_INCOME<11000 -- Problem 2: List community areas with per capita income less than 11000.
+-- SELECT CASE_NUMBER FROM ChicagoCrimeData WHERE DESCRIPTION LIKE '%MINOR%' --Problem 3: List all case numbers for crimes involving minors?(children are not considered minors for the purposes of crime analysis)
+-- SELECT CASE_NUMBER FROM ChicagoCrimeData WHERE DESCRIPTION LIKE '%CHILD%' AND PRIMARY_TYPE='KIDNAPPING' -- Problem 4: List all kidnapping crimes involving a child?
+-- SELECT DISTINCT(PRIMARY_TYPE), LOCATION_DESCRIPTION FROM ChicagoCrimeData WHERE LOCATION_DESCRIPTION LIKE '%SCHOOL%' -- Problem 5: What kinds of crimes were recorded at schools?
+-- SELECT AVG(SAFETY_SCORE) AS AVERAGE_SAFETY_SCORE FROM ChicagoPublicSchools -- Problem 6: what is the average safety score in chicago public schools
+-- select TOP(5) COMMUNITY_AREA_NAME, PERCENT_HOUSEHOLDS_BELOW_POVERTY from ChicagoCensusData ORDER BY PERCENT_HOUSEHOLDS_BELOW_POVERTY DESC -- Problem 7: List 5 community areas with highest % of households below poverty line
+-- SELECT TOP 1 COMMUNITY_AREA_NUMBER ,COUNT(COMMUNITY_AREA_NUMBER) AS CRIME_FREQUENCY FROM ChicagoCrimeData GROUP BY COMMUNITY_AREA_NUMBER ORDER BY COUNT(COMMUNITY_AREA_NUMBER) DESC -- Problem 8: Which community area is most crime prone?
+-- SELECT COMMUNITY_AREA_NAME, HARDSHIP_INDEX FROM ChicagoCensusData WHERE HARDSHIP_INDEX = (SELECT MAX(HARDSHIP_INDEX) FROM ChicagoCensusData) -- Problem 9: Use a sub-query to find the name of the community area with highest hardship index
+-- SELECT COMMUNITY_AREA_NAME FROM ChicagoCensusData WHERE COMMUNITY_AREA_NUMBER = (SELECT TOP 1 COMMUNITY_AREA_NUMBER FROM ChicagoCrimeData GROUP BY COMMUNITY_AREA_NUMBER ORDER BY COUNT(COMMUNITY_AREA_NUMBER) DESC) Problem 10: Use a sub-query to determine the Community Area Name with most number of crimes?
